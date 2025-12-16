9a33f59f2b3d93abe39f44571010d63f6e007157
Mary Ronan <36551329+MazontheMoon@users.noreply.github.com>
2025-12-16 10:50:07 +0000

    add displayWelcome
    Method to display welcome message on screen

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        //greeting
-        //get input
+        //Define variables
+        String employeeID;
+        String firstName;
+        String lastName;
+        int hoursWorked;
+        double payRate;
+        double taxRate;
+        double grossPay;
+        double netPay;
+        double bonusPay;
+
+        //Display greeting
+        displayGreeting();
+
+        //Get input
+        getPayrollInput();
         //validate input
         //calculations
-        //display
+        //display payslip on screen
         //write file
         //closures
     }
+
+    public static void displayGreeting(){
+        System.out.println("===============================");
+        System.out.println("Welcome to the Payroll System");
+        System.out.println("===============================");
+    }
+
+    public static void getPayrollInput(){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+
 }
