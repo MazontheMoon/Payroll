806ebc61d6053269b70ffc337a59918bbdd26deb
Mary Ronan <36551329+MazontheMoon@users.noreply.github.com>
2025-12-16 10:34:04 +0000

    init repo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        //greeting
+        //get input
+        //validate input
+        //calculations
+        //display
+        //write file
+        //closures
+    }
+}
